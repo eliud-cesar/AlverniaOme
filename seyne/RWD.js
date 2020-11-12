2a54198a174502c91649fd505d8e62313d50e1ca
Cesar Eliud <man2personation@gmail.com>
2020-11-12 08:14:56 -0600

    Areglo de RWD
    Se deja el modelo de rwd con solo uno, por motivo de fañta de
    apremdizaje de CSS AVAMZADO.

---
+++ seyne/RWD.js
@@ -0,0 +1,8 @@
+const rwd = document.getElementById('body')
+
+
+// switch (rwd) {
+//     case style.width="360px":
+//         rwd.style.background-image.url("../recursos/PROXIMAMENTE/PROXI-IPHONE-SE.svg")
+//         break
+// }
